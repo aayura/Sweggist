0ccdaebabb94589b43465acded0ee40d6e5df4ee
Retr0_D3DSEC <47493043+aayura@users.noreply.github.com>
2022-01-27 16:08:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -583,8 +583,7 @@ async def check_suggestions(ctx):
 async def rem_suggestions(ctx, msg_id: int):
     cursor.execute(f"DELETE FROM suggestions WHERE msg_id = {msg_id}")
     db.commit()
-    await ctx.send("Suggestion Removed.")
-    
+    await ctx.send("Suggestion Removed.")  
 
 keep_alive()
 TOKEN = os.environ['DISCORD_BOT_TOKEN']

2c55de9c54cdeb4ed4ad8d7381b501268ee0330c
Retr0_D3DSEC <47493043+aayura@users.noreply.github.com>
2022-01-27 12:36:47 +0530

    help fixed

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from discord_slash.utils.manage_commands import create_choice, create_option
 intents = discord.Intents.all()
 intents.members = True
 
-client = commands.Bot(command_prefix='sw',
+client = commands.Bot(command_prefix='$',
                       case_insensitive=True,
                       intents=intents, help_command=None)
 
@@ -19,15 +19,15 @@ slash = SlashCommand(client)
 async def on_ready():
     print("Bot is ready.")
     activity = discord.Activity(type=discord.ActivityType.watching,
-                                name="For sw_helpme")
+                                name="For $help")
     await client.change_presence(status=discord.Status.dnd, activity=activity)
 
 @client.command()
 async def help(ctx):
-    embed = discord.Embed(title='Commands', description='These are the commands available to you at the moment.', color = discord.color.Green())
+    embed = discord.Embed(title='Commands', description='These are the commands available to you at the moment.', color = discord.Colour.Green())
     embed.add_field(name='help', value='Shows this embed.', inline = True)
     await ctx.send(embed=embed)
-    
+
 # just for committing issues.
 
 keep_alive()
